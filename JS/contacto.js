588c04140595123f20b4d7dd718ac34254c1bc20
Tania Polania <tania9415@gmail.com>
2022-01-09 23:44:17 -0300

    final

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,15 +1,29 @@
 $(document).ready(function () {
-
-    $( "form" ).submit(function( event ) {
-        $("#datos-recibidos").removeClass("ocultar")
-        .fadeIn(2000)
-        .delay(3000)
-        .fadeOut( "slow", function() {
-            $("#datos-recibidos").addClass("ocultar")
-            $("form").trigger('reset');
-          });
-        event.preventDefault();
+  $("form").submit(function (event) {
+    $("#datos-recibidos")
+      .removeClass("ocultar")
+      .fadeIn(2000)
+      .delay(3000)
+      .fadeOut("slow", function () {
+        $("#datos-recibidos").addClass("ocultar");
+        $("form").trigger("reset");
       });
-
-
+    event.preventDefault();
+  });
 });
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 4.6641687, lng: -74.060889 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
--- JS/productos.js
+++ JS/productos.js
@@ -96,6 +96,11 @@ $(document).ready(function () {
     }
   }
 
+  function validarForm() {
+    console.log("***", this);
+    return true;
+  }
+
   $.ajax({
     method: "GET",
     url: "../JS/productos.json",
@@ -111,16 +116,36 @@ $(document).ready(function () {
             <div class="card-body">
                 <h5 class="card-title">${productos[i].nombre}</h5>
                 <ul class="card-text" id="caracteristicas-${i}"></ul>
-                <div class="input-group mb-3">
-                    <input id="cantidad-${i}" type="text" class="form-control" placeholder="Cantidad" aria-label="Cantidad" aria-describedby="boton-agregar">
-                    <button id="btn-${i}" type="button" class="btn btn-dark" >Agregar al carrito</button>
+                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                  </symbol>
+                </svg>
+                <div id="agregar-alerta-${i}" class="alert alert-warning d-flex align-items-center ocultar" role="alert">
+                  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                  <div>
+                    Este campo es requerido
+                  </div>
                 </div>
-
+                <div class="input-group mb-3">
+                  <input id="cantidad-${i}" type="text" class="form-control" placeholder="Cantidad" aria-label="Cantidad" aria-describedby="boton-agregar" required>
+                  <button id="btn-${i}" type="button" class="btn btn-dark" >Agregar al carrito</button>
+                </div>
             </div>
         </div>`
         );
         productList.append(productoHijo);
         $(`#btn-${i}`).click(function () {
+          if ($(`#cantidad-${i}`).val().trim() == "") {
+            $(`#agregar-alerta-${i}`)
+              .removeClass("ocultar")
+              .fadeIn(2000)
+              .delay(3000)
+              .fadeOut("slow", function () {
+                $(`#agregar-alerta-${i}`).addClass("ocultar");
+              });
+            return;
+          }
           agregarAlCarrito(i, $(`#cantidad-${i}`).val());
         });
 
